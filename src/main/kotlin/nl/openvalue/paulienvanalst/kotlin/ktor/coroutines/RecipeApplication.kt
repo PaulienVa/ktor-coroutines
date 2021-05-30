bc79c450dd1ca7e8bd15a3937e35c4d6c9ef650b
Paulien van Alst <coding@paulienvanalst.com>
2021-05-30 20:41:29 +0200

    Adding standard configuration

--- src/main/kotlin/nl/openvalue/paulienvanalst/kotlin/ktor/coroutines/RecipeApplication.kt
+++ src/main/kotlin/nl/openvalue/paulienvanalst/kotlin/ktor/coroutines/RecipeApplication.kt
@@ -1,4 +1,29 @@
 package nl.openvalue.paulienvanalst.kotlin.ktor.coroutines
 
-class RecipeApplication {
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.jackson.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(
+        Netty,
+        port = 8080,
+        module = Application::recipes
+    ).start(wait = true)
+}
+
+
+fun Application.recipes() {
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+        }
+    }
+
+//    install(Routing) {
+//        recipeEndpoint()
+//    }
 }
